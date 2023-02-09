dd32fdd233cee23eeb00ae09400f450f6f2f12e8
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-09 22:39:52 +0200

    Create HomePage.jsx

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,10 @@
+export const HomePage = () => {
+  return (
+    <main>
+      <div className="container">
+        <h1 className="title">Trending today</h1>
+        {/* <MovieList /> */}
+      </div>
+    </main>
+  );
+};
