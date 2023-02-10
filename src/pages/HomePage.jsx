900c4b79f93b5d3a9fd9ec604e7f7bbe76e56650
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-10 16:12:55 +0200

    Update HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,44 @@
+import { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Loader } from 'components/Loader/Loader';
+import { toast } from 'react-toastify';
+import { getTrending } from 'services/api/fetchApi';
+
+import { MoviesList } from 'components/MoviesList/MoviesList';
+
 export const HomePage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getTrending()
+      .then(({ results }) => {
+        const movies = results.map(({ id, original_title, poster_path }) => {
+          return { id, original_title, poster_path };
+        });
+
+        setItems([...movies]);
+      })
+      .catch(error => {
+        setError(error);
+      })
+      .finally(() => setIsLoading(false));
+  }, [setItems]);
+
   return (
     <main>
       <div className="container">
         <h1 className="title">Trending today</h1>
-        {/* <MovieList /> */}
+        {isLoading && <Loader />}
+        {error &&
+          toast.error('Somthing wrong. Try again', {
+            theme: 'colored',
+            position: 'top-center',
+          })}
+        <MoviesList movies={items} />
       </div>
     </main>
   );
