a7279a50e12f7999106f37517c611613e6004f92
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-10 16:12:49 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,18 @@ import { Routes, Route } from 'react-router-dom';
 
 import { HomePage } from 'pages/HomePage';
 import { MoviesPage } from 'pages/MoviesPage';
-import { Header } from './Header/Header';
+import { MovieInfoPage } from 'pages/MovieInfoPage';
+import { Navbar } from './Navbar/Navbar';
 import '../index.css';
 
 export const App = () => {
   return (
     <>
-      <Header />
+      <Navbar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/movies" element={<MoviesPage />} />
+        <Route path="/movies/:id" element={<MovieInfoPage />} />
       </Routes>
     </>
   );
