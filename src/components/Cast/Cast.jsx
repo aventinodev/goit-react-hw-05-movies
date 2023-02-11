e78e2acbf5f07879d5ee574899a120ae5f3765ae
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-11 15:06:02 +0200

    Update Cast.jsx

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,7 +1,8 @@
+import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { getMovieCredits } from 'services/api/fetchApi';
-import { toast } from 'react-toastify';
+
 import NoImageAvailable from '../../images/NoImageAvailable.jpg';
 import css from 'components/Cast/Cast.module.css';
 
@@ -23,30 +24,43 @@ const Cast = () => {
       .finally(setIsLoading(false));
   }, [id]);
   const pathImg = 'https://image.tmdb.org/t/p/w300/';
+
   return (
-    <ul className={css.list}>
+    <>
       {isLoading && <Loader />}
-      {error &&
-        toast.error('Somthing wrong. Try again', {
-          theme: 'colored',
-          position: 'top-center',
-        })}
-      {cast.map(({ id, profile_path, original_name, character }) => (
-        <li key={id} className={css.item}>
-          <div className={css.imgWrap}>
-            <img
-              src={
-                profile_path ? `${pathImg}${profile_path}` : NoImageAvailable
-              }
-              alt={original_name}
-              width="200"
-            />
-          </div>
-          <p className={css.text}>{original_name}</p>
-          <p className={css.text}>Character: {character}</p>
-        </li>
-      ))}
-    </ul>
+      {error && alert('Somthing wrong. Try again')}
+
+      {
+        !cast.length ? (
+          <p>No information</p>
+        ) : (
+          <ul className={css.list}>
+            {cast.map(({ id, profile_path, original_name, character }) => (
+              <li key={id} className={css.item}>
+                <div className={css.imgWrap}>
+                  <img
+                    src={
+                      profile_path
+                        ? `${pathImg}${profile_path}`
+                        : NoImageAvailable
+                    }
+                    alt={original_name}
+                    width="200"
+                  />
+                </div>
+                <p className={css.text}>{original_name}</p>
+                <p className={css.text}>Character: {character}</p>
+              </li>
+            ))}
+          </ul>
+        )
+
+        //   <p className={css.text}>No information</p>
+      }
+    </>
   );
+};
+Cast.propTypes = {
+  id: PropTypes.number.isRequired,
 };
 export default Cast;
