6fb7a5a95dff2864b80e5d6765c55fc735797915
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-11 06:36:28 +0200

    Update Navbar.jsx

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,21 +7,21 @@ import css from './Navbar.module.css';
 // };
 export const Navbar = () => {
   return (
-    <div className={css.wrapper}>
-      <div className="container">
-        <ul className={css.nav}>
-          <li>
-            <NavLink className={css.link} to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className={css.link} to="/movies">
-              Movies
-            </NavLink>
-          </li>
-        </ul>
-      </div>
+    // <div className={css.wrapper}>
+    <div className="container">
+      <ul className={css.nav}>
+        <li>
+          <NavLink className={css.link} to="/" end>
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink className={css.link} to="/movies">
+            Movies
+          </NavLink>
+        </li>
+      </ul>
     </div>
+    // </div>
   );
 };
