485c46f437668d85927d8b1058cd88d133898700
Olena Bondar <olena.bondar1060@gmail.com>
2023-02-11 05:18:05 +0200

    Update MoviesPage.jsx

--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,9 +1,51 @@
+import { useState, useEffect } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { Loader } from 'components/Loader/Loader';
+
 import { SearchMovie } from 'components/SearchMovie/SearchMovie';
+import { MoviesList } from 'components/MoviesList/MoviesList';
+import { searchMovies } from 'services/api/fetchApi';
+import { showError } from 'services/utils/showError';
 
 export const MoviesPage = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [items, setItems] = useState([]);
+
+  const [searchParams, setSearchParams] = useSearchParams();
+  const query = searchParams.get('query');
+
+  useEffect(() => {
+    if (!query) {
+      return;
+    }
+    setIsLoading(true);
+    searchMovies(query)
+      .then(({ results }) => {
+        setItems([...results]);
+        if (!results.length) {
+          showMessage();
+          setSearchParams({ query: '' });
+        }
+      })
+      .catch(error => setError(error.message))
+      .finally(() => setIsLoading(false));
+  }, [query]);
+
+  const handleFormSubmit = query => {
+    setSearchParams({ query: query });
+    setItems([]);
+  };
+
+  const showMessage = query => {
+    return alert('We did not find such movie');
+  };
   return (
     <main>
-      <SearchMovie />
+      {error && showError()}
+      <SearchMovie onSubmit={handleFormSubmit} />
+      {isLoading && <Loader />}
+      <MoviesList movies={items} />
     </main>
   );
 };
